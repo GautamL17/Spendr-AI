014b2b13999a00b8f3649e6dfc2e87ee3eccf089
Gautam Lodwal <115150191+GautamL17@users.noreply.github.com>
2026-03-29 16:59:24 +0530

    Delete core directory

--- core/api/v1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include 
- 
-urlpatterns = [
-    path('', include('expenses.urls')),
-    path('users/', include('users.urls')),
-    path('reports/', include('reports.urls'))
-]
